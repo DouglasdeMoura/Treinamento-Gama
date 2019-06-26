f77aa4abee54f50d5e9ab254d76cf48b915964d3
Douglas Moura <douglas.ademoura@gmail.com>
2019-06-26 20:55:49 -0300

    Update sumall.js

--- dia1/exercicio2/sumall.js
+++ dia1/exercicio2/sumall.js
@@ -1,5 +1,18 @@
+// Soma todos os números do array
 function sumAll(arr) {
   return arr.reduce((a, b) => a + b);
 }
 
-console.log(sumAll([1,2,3,4,5])); // 15
+// Soma o intervalo entre os números
+
+function sumInterval(arr) {
+  var array = [];
+  limit = arr.pop();
+  for (var i = 0; i < limit; i++) {
+    array[i] = arr[0] + i;
+  }
+
+  return sumAll(array);
+}
+
+sumInterval([1,4]);
