d5e3dcdb57e45d4823e8d78329dc598832da7aa1
Douglas Moura <douglas.ademoura@gmail.com>
2019-06-29 11:11:31 -0300

    Usando funções novas

---
+++ dia4/listas.js
@@ -0,0 +1,42 @@
+var alunos = [
+  {nome: 'Felipe M.', sexo: 'M', idade: 13},
+  {nome: 'Luiza A.', sexo: 'F', idade: 14},
+  {nome: 'Marcos T.', sexo: 'M', idade: 14},
+  {nome: 'Bianca B.', sexo: 'F', idade: 13},
+  {nome: 'Rogério R.', sexo: 'M', idade: 15},
+];
+
+// Listar todos os nomes
+alunos.forEach((aluno) => {
+  return aluno.nome;
+});
+
+// dobrar a idade
+alunos.map((aluno) => {
+  aluno.idade = aluno.idade * 2;
+  return aluno;
+});
+
+// Filtrar somente para o sexo F
+alunos.filter((aluno) => {
+  if ('F' === aluno.sexo) {
+    return aluno;
+  }
+});
+
+// Encontrar um registro (Bianca B.)
+alunos.find((aluno) => {
+  if ('Bianca B.' === aluno.nome) {
+    return aluno;
+  }
+});
+
+// Verificar se TODOS atendem critério (idade > 14)
+alunos.every((aluno) => {
+  return aluno.idade > 14;
+});
+
+// Verificar se ALGUM atende critério (idade > 18)
+alunos.some((aluno) => {
+  return aluno.idade > 18;
+});
